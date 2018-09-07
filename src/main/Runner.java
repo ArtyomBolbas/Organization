ea6051b7033fa38bce7333f08bc16fda58d615be
Alexei Budko <bas2020@inbox.ru>
2018-09-07 15:32:09 +0300

    добавил изменения на рабочем компьютере

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -1,21 +1,22 @@
 package main;
 
-import org.apache.log4j.Logger;
+//import org.apache.log4j.Logger;
 
 public class Runner {
 
-	private final static Logger LOG = Logger.getLogger(Runner.class);
+	//private final static Logger LOG = Logger.getLogger(Runner.class);
 	
 	public static void main(String[] args) {
 		//LOG.info("Проверка корректности - работы библиотеки log4j");
+		System.out.println("Внес изменения");
 	}
 
 	
 	// ----------------------------------------------
 	// сеттеры, геттеры
 	// ----------------------------------------------
-	public static Logger getLog() {
+/*	public static Logger getLog() {
 		return LOG;
-	}
+	}*/
 	
 }
