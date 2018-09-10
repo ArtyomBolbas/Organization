463d7e92db28b1ff9f1e336f9a6c0fb4e5e99626
Artyom Bolbas <artyombolbas@yandex.ru>
2018-09-10 15:52:52 +0300

    add v0.2 "Organization": package:"additional.calsses" -(class) DateOfBirth -(class) RelatedSpecialties package: "model.company" -(class) Company -(interface) AddObject -(interface) Arrange -(interface) Coefficient -(interface) LayOff -(interface) MoveWorkers -(interface) Remove -(interface) Report package: "model.hierarchy.of.employees" -(class(abstract)) Administration -(class(abstract)) Employee -(class(abstract)) Leaders -(class(abstract)) Manging -(class) ChiefEnginner -(class) Engineer -(class) Foreman -(class) Master -(class) Principal -(class) Working

--- src/model/hierarchy/of/employees/Engineer.java
+++ src/model/hierarchy/of/employees/Engineer.java
@@ -7,7 +7,6 @@ public class Engineer extends Managing {
 	// поля, свойства необходимые Engineer
 	// ----------------------------------------------
 	
-	
 	@Override
 	public void remove() {
 		// TODO Auto-generated method stub
